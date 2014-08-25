a5dda112c93dff582fd8b92a381ad88dabbb057e
Chip Bell <chip.bell@spinen.com>
2014-08-24 23:49:57 -0400

    Pulling some code out to templates

---
+++ templates/bad-format.php
@@ -0,0 +1,8 @@
+<section>
+    <h2>Programming Team Lectures</h2>
+    <p>
+        Nice try! <?php echo $_GET['lecture']; ?> doesn't really look like a
+        date does it? Remember, <code>yyyy-mm-dd</code> is the format to use (a date
+        essentially).
+    </p>
+</section>
---
+++ templates/no-lecture.php
@@ -0,0 +1,6 @@
+<section>
+    <h2>Programming Team Lectures</h2>
+    <p>
+        Sorry, but there's not a lecture for <?php echo $_GET['lecture']; ?>.
+    </p>
+</section>
