7a302cb517d2f676e671784fb64d0af7f3633e32
Chip Bell <chip.bell@spinen.com>
2014-08-25 01:03:29 -0400

    Adding comments

--- lib/LectureController.php
+++ lib/LectureController.php
@@ -3,6 +3,9 @@ require_once('../vendor/autoload.php');
 
 class LectureController
 {
+    /**
+     * Creates a new controller, booting up the twig renderer
+     */
     public function __construct()
     {
         $loader = new Twig_Loader_Filesystem($this->templateDir());
@@ -11,12 +14,23 @@ class LectureController
 
     /**
      * Called when no lecture provided
+     *
+     * @param string $lecture The lecture that is in a bad format
+     *
+     * @return void
      */
     public function badFormat($lecture)
     {
-        return $this->render('bad-format.html', compact('lecture'));
+        return $this->render('bad-format.html', ['lecture' => $lecture]);
     }
     
+    /**
+     * Converts the filename of a lecture to a template data that can be rendered in a view
+     * 
+     * @param string $filename The filename to convert to an array
+     * 
+     * @return array
+     */
     protected function convertFileNameToTemplateData($filename)
     {
         $date = str_replace('.html', '', basename($filename));
@@ -27,6 +41,11 @@ class LectureController
         ];
     }
     
+    /**
+     * Gets all lectures in view format (nested arrays)
+     *
+     * @return array
+     */
     protected function getAllLectures()
     {
         $files = [];
@@ -37,6 +56,8 @@ class LectureController
 
     /**
      * Lists all lectures
+     *
+     * @return void
      */
     public function index()
     {
@@ -44,26 +65,62 @@ class LectureController
         return $this->render('index.html', compact('lectures'));
     }
     
+    /**
+     * Returns true if a given lecture is in the correct format to be found on disk
+     * 
+     * @param string $lecture The lecture to check
+     * 
+     * @return bool
+     */
     protected function isLectureFormat($lecture)
     {
         return preg_match('/^\d\d\d\d-\d\d-\d\d$/', $lecture);
     }
 
+    /**
+     * Returns true if the given lecture exists 
+     * 
+     * @param string $lecture The lecture to check
+     * 
+     * @return bool
+     */
     protected function lectureExists($lecture)
     {
         return file_exists($this->templateDir("lectures/$lecture.html"));
     }
 
+    /**
+     * Renders the not found page for a lecture (that apparently doesn't exist) 
+     * 
+     * @param string $lecture The lecture that didn't exist
+     * 
+     * @return void
+     */
     public function lectureNotFound($lecture)
     {
-        return $this->render('no-lecture.html', compact('lecture'));
+        return $this->render('no-lecture.html', ['lecture' => $lecture]);
     }
 
+    /**
+     * Renders a given twig template with the provided variables
+     * 
+     * @param string $template_name      The name of the twig template to render
+     * @param array  $template_variables The variables to bind to the template
+     * 
+     * @return string The string result of rendering the template
+     */
     protected function render($template_name, array $template_variables = [])
     {
         return $this->twig->render($template_name, $template_variables);
     }
 
+    /**
+     * Attempts to render a given lecture
+     * 
+     * @param string $lecture The lecture to render
+     * 
+     * @return string
+     */
     public function showLecture($lecture)
     {
         if (!$this->isLectureFormat($lecture)) {
@@ -77,6 +134,13 @@ class LectureController
         return $this->render("lectures/$lecture.html");
     }
 
+    /**
+     * Builds the path to a template
+     * 
+     * @param string $child_directory An optional child directory to append to the end
+     * 
+     * @return string
+     */
     protected function templateDir($child_directory = '')
     {
         return __DIR__ . '/../templates/' . $child_directory;
