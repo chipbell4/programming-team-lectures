bb643518a68e341b2a023a546bc2c793d0679d0b
Chip Bell <chip.bell@spinen.com>
2014-08-25 00:29:28 -0400

    Adding a method for listing all templates

--- lib/LectureController.php
+++ lib/LectureController.php
@@ -5,8 +5,40 @@ class LectureController
 {
     public function __construct()
     {
-        $loader = new Twig_Loader_Filesystem(__DIR__ . '/../templates');
+        $loader = new Twig_Loader_Filesystem($this->templateDir());
         $this->twig = new Twig_Environment($loader);
+    }
+
+    /**
+     * Lists all lectures
+     */
+    public function index()
+    {
+        $lectures = $this->getAllLectures();
+        return $this->render('index.html', compact('lectures'));
+    }
+
+    protected function getAllLectures()
+    {
+        $files = [];
+        exec('ls ' . $this->templateDir('lectures'), $files);
+
+        return array_map([$this, 'convertFileNameToTemplateData'], $files);
+    }
+
+    protected function templateDir($child_directory = '')
+    {
+        return __DIR__ . '/../templates/' . $child_directory;
+    }
+
+    protected function convertFileNameToTemplateData($filename)
+    {
+        $date = str_replace('.html', '', basename($filename));
+        list($year, $month, $day) = explode('-', $date);
+        return [
+            'query_param' => $date,
+            'pretty_date' => "$month/$day/$year"
+        ];
     }
 
     /**
